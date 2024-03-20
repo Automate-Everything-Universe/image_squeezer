7b1dfb822bcfda0c024f4e2bfcf40b015cf4e1b2
Dragos Josan <dragosjosan@gmail.com>
2024-03-20 19:15:11 +0100

    Add method to fix image orientation.

--- src/image_squeezer/sizer.py
+++ src/image_squeezer/sizer.py
@@ -5,7 +5,7 @@ Module which handles the sizing
 from abc import ABC
 from abc import abstractmethod
 
-from PIL import Image
+from PIL import Image, ImageOps
 
 from .utils_validation import is_number_valid
 
@@ -37,6 +37,7 @@ class AspectRatioSizer(Sizer):
         self._width = is_number_valid(width)
 
     def resize(self) -> Image:
+        self.fix_img_rotation(img=self.image)
         new_height = self._calculate_height(img=self.image)
         resized_img = self.image.resize(
             size=(self._width, new_height), resample=Image.Resampling.LANCZOS
@@ -48,3 +49,7 @@ class AspectRatioSizer(Sizer):
         w_percent = self._width / float(img.size[0])
         new_height = int(float(img.size[1]) * float(w_percent))
         return new_height
+
+    @staticmethod
+    def fix_img_rotation(img: Image) -> Image:
+        return ImageOps.exif_transpose(img)
